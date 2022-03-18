9faba824de6c3c6285052df5c8a0fa80b1497971
mohab-hisham <61351802+mohab-hisham@users.noreply.github.com>
2022-03-18 02:01:00 +0200

    add median and laplacian filters

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ class GUI (Ui_MainWindow):
         self.comboBox_filters.addItem("No filter")
         self.comboBox_filters.addItem("blur")
         self.comboBox_filters.addItem("median")
+        self.comboBox_filters.addItem("laplace")
+
 
         self.figure_Orig_Spat = Figure(figsize=(3, 3), dpi=100)
         self.axes_Orig_Spat = self.figure_Orig_Spat.add_subplot()
@@ -114,8 +116,23 @@ class GUI (Ui_MainWindow):
     def filtering(self): #this function is called when the combobox is changed
 
         ## write the code of the right filter depending on the combobox's current index
+        if self.comboBox_filters.currentText() == 'laplace':
+            laplaceSrc = cv2.GaussianBlur(self.img, (3, 3), 0)
+            filteredLaplacian = cv2.Laplacian(laplaceSrc, cv2.CV_16S, ksize=7)
+            laplaceF = np.fft.fft2(filteredLaplacian)
+            laplaceFshift = np.fft.fftshift(laplaceF)
+            laplace_magnitude_spectrum = 20*np.log(np.abs(laplaceFshift))
+            self.axes_Filt_Spat.imshow(filteredLaplacian, cmap='gray')
+            self.axes_Filt_Freq.imshow(laplace_magnitude_spectrum.astype('uint8'), cmap='gray')
 
-
+        elif self.comboBox_filters.currentText() == 'median':
+            median = cv2.medianBlur(self.img,11)
+            medianF = np.fft.fft2(median)
+            medianFshift = np.fft.fftshift(medianF)
+            median_magnitude_spectrum = 20*np.log(np.abs(medianFshift)) 
+            self.axes_Filt_Spat.imshow(median, cmap='gray')
+            self.axes_Filt_Freq.imshow(median_magnitude_spectrum.astype('uint8'), cmap='gray')
+          
         ##Write filters code here## 
 
 
